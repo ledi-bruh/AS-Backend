7981af458854e40455c8aa02de948bb9583b98d3
andreas <podovinov2003andrey@gmail.com>
2022-12-14 19:36:01 +0600

    add task 2.2

--- tasks/task2.2/main.py
+++ tasks/task2.2/main.py
@@ -1,5 +1,9 @@
+def largest_number(n: int, numbers: str) -> str:
+    return ' '.join(sorted(numbers.split(), key=lambda x: x + '9' * (4 - len(x)), reverse=True))
+
+
 def main():
-    pass
+    print(largest_number(int(input()), input()))
 
 
 if __name__ == '__main__':
