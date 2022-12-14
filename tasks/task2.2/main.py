69e4c70349bbe995c6e76fa858279cf442b880a3
andreas <podovinov2003andrey@gmail.com>
2022-12-14 19:41:39 +0600

    remove unused in task 2.2

--- tasks/task2.2/main.py
+++ tasks/task2.2/main.py
@@ -1,9 +1,9 @@
-def largest_number(n: int, numbers: str) -> str:
+def largest_number(numbers: str) -> str:
     return ' '.join(sorted(numbers.split(), key=lambda x: x + '9' * (4 - len(x)), reverse=True))
 
 
 def main():
-    print(largest_number(int(input()), input()))
+    print(largest_number(input()))
 
 
 if __name__ == '__main__':
