63536c3baaab0b6fa7a37c5d1e3756d7037caf27
andreas <podovinov2003andrey@gmail.com>
2022-12-20 22:18:46 +0600

    fix task 2.2

--- tasks/task2.2/main.py
+++ tasks/task2.2/main.py
@@ -1,5 +1,5 @@
 def largest_number(numbers: str) -> str:
-    return ' '.join(sorted(numbers.split(), key=lambda x: x + x[0] * (4 - len(x)), reverse=True))
+    return ''.join(sorted(numbers.split(), key=lambda x: x + x[-1] * (4 - len(x)), reverse=True))
 
 
 def main():
